2b98a61f0ab7c660bf8bb01419ce632b02a08208
Lafon Christophe <lfnchrstph@MacBook-Pro-de-Lafon.local>
2022-12-03 23:38:59 +0100

    config installé (dotenc, cors, crypto, uid2, mongoose)

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
 
 app.get("/", (req, res) => {
   res.json({ message: "test ok" });

c6ee0b4c31c03b2bba649f1b67cc0b58204bdc23
Lafon Christophe <lfnchrstph@MacBook-Pro-de-Lafon.local>
2022-12-03 16:58:34 +0100

    test back ok

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hi" });
-});
-
-app.get("/hello", (req, res) => {
-  res.json({ message: "Hello" });
+  res.json({ message: "test ok" });
 });
 
 app.listen(3000, () => {

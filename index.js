f4fe6f1bb4f2dc795b2200dfcf3b1382fff386bb
Lafon Christophe <lfnchrstph@MacBook-Pro-de-Lafon.local>
2022-12-07 23:31:24 +0100

    level edit

--- index.js
+++ index.js
@@ -2,14 +2,56 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+app.use(express.json());
 app.use(cors());
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL);
+// mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect("mongodb://localhost:27017/buzzle");
 
-app.get("/", (req, res) => {
-  res.json({ message: "test ok" });
+const Level = mongoose.model("Level", {
+  pattern: Array,
+});
+
+app.post("/test", async (req, res) => {
+  try {
+    console.log("ok");
+    console.log(req.body);
+    res.status(200).json({
+      message: "requête edit accordée",
+      level: newLevel,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/levels", async (req, res) => {
+  try {
+    const levels = await Level.find();
+    res.status(200).json({
+      message: "requête levels accordée",
+      levels: levels,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  try {
+    const newLevel = new Level({
+      pattern: req.body.pattern,
+    });
+    await newLevel.save();
+    res.status(200).json({
+      message: "requête edit accordée",
+      level: newLevel,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 app.listen(3000, () => {
